3afd31aa004dc7492c6a8bc70dec56da7b62cd51
Atulnegi10 <natul8514@gmail.com>
2026-07-13 22:33:29 +0530

    completed todo app

--- Todo app/index.js
+++ Todo app/index.js
@@ -1,23 +1,22 @@
 const express = require("express");
-const jwt = require("javawebtoken");
+const jwt = require("jsonwebtoken");
 const path = require("path");
-
 const app = express();
 
 app.use(express.json());
 const JWT_SECRET = "randomatul";
 const users = [];
 
-app.get("/",function(req,res){
-  res.sendFile(path.join(__dirname+"index.html"));
-})
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/signup", function(req, res) {
 
     const username = req.body.username;
     const password = req.body.password;
-
-    const foundUser = users.find(u => u.username === username);
+ const foundUser = users.find(u => u.username === username);
 
     if (foundUser) {
         return res.status(400).json({
@@ -34,62 +33,87 @@ app.post("/signup", function(req, res) {
     res.json({
         message: "Signup successful"
     });
+
 });
 
-app.post("/signin",function(req,res){
+// Signin
+app.post("/signin", function(req, res) {
 
-    const usernme = req.body.usernmae;
+    const username = req.body.username;
     const password = req.body.password;
 
-    const founduser = users.find(u => u.username === username && u.password === password);
+    const foundUser = users.find(
+        u => u.username === username &&
+             u.password === password
+    );
 
-    if(founduser){
-        const token = jwt.sign({username},JWT_SECRET);
+    if (foundUser) {
+
+        const token = jwt.sign(
+            { username },
+            JWT_SECRET
+        );
 
         res.json({
             token: token
         });
-    }else{
-        res.json({
-            message:"invalid username or password"
-        })
+
+    } else {
+
+        res.status(403).json({
+            message: "Invalid username or password"
+        });
+
     }
+
 });
 
-function auth(req,res,next){
+// Authentication Middleware
+function auth(req, res, next) {
+
     const token = req.headers.token;
 
-    const decoded = jwt.verify(token,JWT_SECRET);
-    req.usernmae = decoded.username;
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    req.username = decoded.username;
+
     next();
+
 }
 
-app.post("/todo",function(req,res){
+// Create Todo
+app.post("/todo", auth, function(req, res) {
 
     const title = req.body.title;
 
-    const user = users.find(u => u.username === req.username);
+    const user = users.find(
+        u => u.username === req.username
+    );
 
-    user.todos.push(({
-        id:Date.now(),
-        title:title,
+    user.todos.push({
+        id: Date.now(),
+        title: title,
         done: false
-    }));
+    });
 
     res.json({
-        message: "todo added"
-    })
+        message: "Todo added"
+    });
+
 });
 
-app.get("/todo",function(req,res){
+// Get Todos
+app.get("/todo", auth, function(req, res) {
 
-    const user = users.find(u => u.username === req.username);
+    const user = users.find(
+        u => u.username === req.username
+    );
 
-    res.json(
-        user.todos
-    )
+    res.json(user.todos);
+
 });
 
+// Mark Todo Done
 app.put("/todo", auth, function(req, res) {
 
     const id = req.body.id;
@@ -103,9 +127,11 @@ app.put("/todo", auth, function(req, res) {
     );
 
     if (!todo) {
+
         return res.status(404).json({
             message: "Todo not found"
         });
+
     }
 
     todo.done = true;
@@ -114,4 +140,58 @@ app.put("/todo", auth, function(req, res) {
         message: "Todo marked as done"
     });
 
+});
+
+// Delete Todo
+app.delete("/todo", auth, function(req, res) {
+
+    const id = req.body.id;
+
+    const user = users.find(
+        u => u.username === req.username
+    );
+
+    user.todos = user.todos.filter(
+        todo => todo.id !== id
+    );
+
+    res.json({
+        message: "Todo deleted"
+    });
+
+});
+
+// Update Todo
+app.put("/todo/update", auth, function(req, res) {
+
+    const id = req.body.id;
+    const newTitle = req.body.title;
+
+    const user = users.find(function(u) {
+        return u.username === req.username;
+    });
+
+    const todo = user.todos.find(function(t) {
+        return t.id === id;
+    });
+
+    if (!todo) {
+
+        return res.status(404).json({
+            message: "Todo not found"
+        });
+
+    }
+
+    todo.title = newTitle;
+
+    res.json({
+        message: "Todo updated"
+    });
+
+});
+
+
+app.listen(3000, function() {
+    console.log("Server running on http://localhost:3000");
 });
