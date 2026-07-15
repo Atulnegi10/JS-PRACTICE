8de0fcd5019ab3b98c72447a47fc92d3eb0c01e6
Atulnegi10 <natul8514@gmail.com>
2026-07-15 09:47:17 +0530

    STARTED TODO APP WITH MONGO DB DATABASE

--- todoappwdb/index.js
+++ todoappwdb/index.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const {UserModel,TodoModel} = require("./db")
-const app = express();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "atul123";
 
-app.post("/signup", function(req,res){
-  
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function(req,res){
+   const email = req.body.email;
+   const password = req.body.password;
+   const name = req.body.name;
+
+   await UserModel.create({
+    email: email,
+    password: password,
+    name : name
+   });
+
+   res.json({
+    message: "user logged in"
+   })
+
 });
 
-app.post("/signin", function(req,res){
-  
+app.post("/signin", async function(req,res){
+   const email = req.body.email;
+   const password = req.body.password;
+
+   const user =  await UserModel.findOne({
+    email : email,
+    password : password
+   });
+
+   if(user){
+    const token = jwt.sign({
+       id :user._id
+    },JWT_SECRET);
+    res.json({
+      token : token
+    })
+   }else{
+    res.status(403).json({
+      message : "incorrect credentials"
+    })
+   }
 });
 
 app.post("/todos", function(req,res){
@@ -17,5 +53,26 @@ app.post("/todos", function(req,res){
 app.get("/todos", function(req,res){
   
 });
+
+function auth(req,res,next){
+
+  const token = req.headers.token;
+  const response = jwt.verify(token,JWT_SECRET);
+
+  if(response){
+    req.userID = token.userID;
+    next();
+  }else{
+    res.status(403).json({
+      message : "incorrect credentials"
+    })
+  }
+  }
+
+  module.exports = {
+    auth,
+    JWT_SECRET
+}
+
 
 app.listen(3000);
